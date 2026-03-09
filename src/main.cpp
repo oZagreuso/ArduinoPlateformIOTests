a3b09a29f9fbcd7dc7929975736655fa400c82ca
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2026-03-09 11:50:56 +0100

    addExternalLEDONOFFAPIWIFI

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,72 @@ const char* password = "";
 
 ESP8266WebServer server(80);
 
-const uint8_t LED_PIN = LED_BUILTIN;
+const uint8_t LED_GREEN = D5;
+const uint8_t LED_RED = D6;
+
 bool ledOn = false;
 
+enum SystemMode {
+  MODE_BOOTING,
+  MODE_WIFI_CONNECTING,
+  MODE_WIFI_DISCONNECTED,
+  MODE_READY
+};
+
+SystemMode systemMode = MODE_BOOTING;
+
+unsigned long lastBlinkMs = 0;
+bool blinkState = false;
+
+unsigned long activityUntilMs = 0;
+
+void setLeds(bool green, bool red) {
+  digitalWrite(LED_GREEN, green ? HIGH : LOW);
+  digitalWrite(LED_RED, red ? HIGH : LOW);
+}
+
+void pulseActivity(unsigned long durationMs = 120) {
+  activityUntilMs = millis() + durationMs;
+}
+
+void updateStatusLeds() {
+  unsigned long now = millis();
+
+  if (activityUntilMs > now) {
+    setLeds(true, true);
+    return;
+  }
+
+  switch (systemMode) {
+    case MODE_BOOTING:
+    case MODE_WIFI_CONNECTING:
+      if (now - lastBlinkMs >= 180) {
+        lastBlinkMs = now;
+        blinkState = !blinkState;
+      }
+      setLeds(blinkState, !blinkState);
+      break;
+
+    case MODE_WIFI_DISCONNECTED:
+      if (now - lastBlinkMs >= 400) {
+        lastBlinkMs = now;
+        blinkState = !blinkState;
+      }
+      setLeds(false, blinkState);
+      break;
+
+    case MODE_READY:
+      if (ledOn) {
+        setLeds(true, false);
+      } else {
+        setLeds(false, true);
+      }
+      break;
+  }
+}
+
 void applyLed() {
-  digitalWrite(LED_PIN, ledOn ? LOW : HIGH);
+  updateStatusLeds();
 }
 
 String jsonState() {
@@ -22,6 +83,7 @@ String jsonState() {
   s += "\"led\":"; s += (ledOn ? "true" : "false");
   s += ",\"ip\":\""; s += WiFi.localIP().toString(); s += "\"";
   s += ",\"rssi\":"; s += WiFi.RSSI();
+  s += ",\"wifi_connected\":"; s += (WiFi.status() == WL_CONNECTED ? "true" : "false");
   s += "}";
   return s;
 }
@@ -47,8 +109,6 @@ String getWeather() {
   String url =
     "http://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&current_weather=true";
 
-  Serial.println("[weather] GET " + url);
-
   if (!http.begin(client, url)) {
     return "{\"error\":\"http.begin failed\"}";
   }
@@ -57,7 +117,6 @@ String getWeather() {
   http.setUserAgent("ESP8266");
 
   int httpCode = http.GET();
-  Serial.printf("[weather] httpCode=%d\n", httpCode);
 
   if (httpCode <= 0) {
     String err = http.errorToString(httpCode);
@@ -71,13 +130,13 @@ String getWeather() {
   }
 
   String payload = http.getString();
-  Serial.printf("[weather] bytes=%d\n", payload.length());
-
   http.end();
   return payload;
 }
 
 void handleWifiScan() {
+  pulseActivity();
+
   int n = WiFi.scanNetworks();
   String json = "[";
 
@@ -141,21 +200,28 @@ void sendFileOr404(const char* path, const char* type) {
 }
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  ledOn = false;
-  applyLed();
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  setLeds(false, false);
 
   Serial.begin(115200);
   Serial.println();
+
+  systemMode = MODE_WIFI_CONNECTING;
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   Serial.print("Connexion WiFi");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(400);
+    updateStatusLeds();
+    delay(120);
     Serial.print(".");
   }
+
+  systemMode = MODE_READY;
+  updateStatusLeds();
+
   Serial.println("\nWiFi connecté");
   Serial.print("IP : ");
   Serial.println(WiFi.localIP());
@@ -163,10 +229,9 @@ void setup() {
   initTimeParis();
 
   for (int i = 0; i < 30 && !timeSynced(); i++) {
+    updateStatusLeds();
     delay(200);
   }
-  Serial.print("NTP synced: ");
-  Serial.println(timeSynced() ? "YES" : "NO");
 
   if (!LittleFS.begin()) {
     Serial.println("Erreur LittleFS");
@@ -174,29 +239,77 @@ void setup() {
     Serial.println("LittleFS OK");
   }
 
-  server.on("/", []() { sendFileOr404("/index.html", "text/html; charset=utf-8"); });
-  server.on("/wifi", []() { sendFileOr404("/wifi.html", "text/html; charset=utf-8"); });
-  server.on("/console", []() { sendFileOr404("/console.html", "text/html; charset=utf-8"); });
+  server.on("/", []() {
+    pulseActivity();
+    sendFileOr404("/index.html", "text/html; charset=utf-8");
+  });
 
-  server.on("/app.css", []() { sendFileOr404("/app.css", "text/css; charset=utf-8"); });
-  server.on("/app.js", []() { sendFileOr404("/app.js", "application/javascript; charset=utf-8"); });
+  server.on("/wifi", []() {
+    pulseActivity();
+    sendFileOr404("/wifi.html", "text/html; charset=utf-8");
+  });
 
-  server.on("/api/time", []() { server.send(200, "application/json; charset=utf-8", jsonTime()); });
-  server.on("/api/system", []() { server.send(200, "application/json; charset=utf-8", jsonSystem()); });
-  server.on("/api/state", []() { server.send(200, "application/json; charset=utf-8", jsonState()); });
+  server.on("/console", []() {
+    pulseActivity();
+    sendFileOr404("/console.html", "text/html; charset=utf-8");
+  });
 
-  server.on("/api/on", []() { ledOn = true; applyLed(); server.send(200, "application/json; charset=utf-8", jsonState()); });
-  server.on("/api/off", []() { ledOn = false; applyLed(); server.send(200, "application/json; charset=utf-8", jsonState()); });
-  server.on("/api/toggle", []() { ledOn = !ledOn; applyLed(); server.send(200, "application/json; charset=utf-8", jsonState()); });
+  server.on("/app.css", []() {
+    pulseActivity();
+    sendFileOr404("/app.css", "text/css; charset=utf-8");
+  });
+
+  server.on("/app.js", []() {
+    pulseActivity();
+    sendFileOr404("/app.js", "application/javascript; charset=utf-8");
+  });
+
+  server.on("/api/time", []() {
+    pulseActivity();
+    server.send(200, "application/json; charset=utf-8", jsonTime());
+  });
+
+  server.on("/api/system", []() {
+    pulseActivity();
+    server.send(200, "application/json; charset=utf-8", jsonSystem());
+  });
+
+  server.on("/api/state", []() {
+    pulseActivity();
+    server.send(200, "application/json; charset=utf-8", jsonState());
+  });
+
+  server.on("/api/on", []() {
+    pulseActivity();
+    ledOn = true;
+    applyLed();
+    server.send(200, "application/json; charset=utf-8", jsonState());
+  });
+
+  server.on("/api/off", []() {
+    pulseActivity();
+    ledOn = false;
+    applyLed();
+    server.send(200, "application/json; charset=utf-8", jsonState());
+  });
+
+  server.on("/api/toggle", []() {
+    pulseActivity();
+    ledOn = !ledOn;
+    applyLed();
+    server.send(200, "application/json; charset=utf-8", jsonState());
+  });
 
   server.on("/api/wifi", handleWifiScan);
 
   server.on("/api/weather", []() {
+    pulseActivity();
     String weather = getWeather();
     server.send(200, "application/json; charset=utf-8", weather);
   });
 
   server.onNotFound([]() {
+    pulseActivity();
     server.send(404, "text/plain; charset=utf-8", "404");
   });
 
@@ -205,5 +318,16 @@ void setup() {
 }
 
 void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    if (systemMode != MODE_READY) {
+      systemMode = MODE_READY;
+    }
+  } else {
+    if (systemMode != MODE_WIFI_DISCONNECTED) {
+      systemMode = MODE_WIFI_DISCONNECTED;
+    }
+  }
+
+  updateStatusLeds();
   server.handleClient();
 }
